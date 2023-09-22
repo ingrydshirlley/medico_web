9b9b4f7a8e30ed224969bc313c534cd878d8f1bb
Ingryd Shirlley <ingryddasilva7@gmail.com>
2023-09-22 11:25:42 -0300

    refinei o perfil e prontuatrio pronto

--- pages/dieta.js
+++ pages/dieta.js
@@ -1,11 +1,35 @@
-// Dieta.js
 import React from 'react';
 import styles from './../styles/Dieta.module.css';
-
+import TituloSecao from '@/components/TituloSecao';
+import ConversaPaciente from '@/components/ConversaPaciente';
 const Dieta = () => {
     return (
         <div className={styles['dieta-container']}>
-            <h1 className={styles['dieta-header']}>Página de dieta</h1>
+            <TituloSecao title="Gerenciar Dietas" />
+
+            <div className={styles['container-geral']}>
+                
+                <div className={styles['container-pacientes']}>
+                    <div className={styles['pacientes-container']}>
+                        <input type="text" placeholder='Pesquisar um paciente' />
+
+                        <div className={styles['pacientes']}>
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                            <ConversaPaciente />
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className={styles['container-conversa']}>
+
+                </div> */}
+            </div>
         </div>
     );
 };
